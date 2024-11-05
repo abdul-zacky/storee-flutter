c02afb6e1d902a562e4cf5433aa20f6a8b5d7af0
abdul-zacky <zacxvan@gmail.com>
2024-11-05 10:44:59 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:storee/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Storee',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepPurple,
+        ).copyWith(secondary: Colors.deepPurple[400]),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
---
+++ lib/menu.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final String npm = '2306214510'; // NPM
+    final String name = 'Abdul Zacky'; // Nama
+    final String className = 'PBP C'; // Kelas
+    final List<ItemHomepage> items = [
+      ItemHomepage("Lihat Daftar Produk", Icons.production_quantity_limits),
+      ItemHomepage("Tambah Produk", Icons.add),
+      ItemHomepage("Logout", Icons.logout),
+    ];
+    return Scaffold(
+      // AppBar adalah bagian atas halaman yang menampilkan judul.
+      appBar: AppBar(
+        title: const Text(
+          'Storee',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      // Body halaman dengan padding di sekelilingnya.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // Menyusun widget secara vertikal dalam sebuah kolom.
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Row untuk menampilkan 3 InfoCard secara horizontal.
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InfoCard(title: 'NPM', content: npm),
+                InfoCard(title: 'Name', content: name),
+                InfoCard(title: 'Class', content: className),
+              ],
+            ),
+
+            // Memberikan jarak vertikal 16 unit.
+            const SizedBox(height: 16.0),
+
+            // Menempatkan widget berikutnya di tengah halaman.
+            Center(
+              child: Column(
+                // Menyusun teks dan grid item secara vertikal.
+
+                children: [
+                  // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Welcome to Storee',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+
+                  // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
+                  GridView.count(
+                    primary: true,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    // Agar grid menyesuaikan tinggi kontennya.
+                    shrinkWrap: true,
+
+                    // Menampilkan ItemCard untuk setiap item dalam list items.
+                    children: List.generate(items.length, (index) {
+                      return ItemCard(item: items[index], index: index);
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  // Kartu informasi yang menampilkan title dan content.
+
+  final String title; // Judul kartu.
+  final String content; // Isi kartu.
+
+  const InfoCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // Membuat kotak kartu dengan bayangan dibawahnya.
+      elevation: 2.0,
+      child: Container(
+        // Mengatur ukuran dan jarak di dalam kartu.
+        width: MediaQuery.of(context).size.width /
+            3.5, // menyesuaikan dengan lebar device yang digunakan.
+        padding: const EdgeInsets.all(16.0),
+        // Menyusun title dan content secara vertikal.
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemHomepage {
+  final String name;
+  final IconData icon;
+
+  ItemHomepage(this.name, this.icon);
+}
+
+class ItemCard extends StatelessWidget {
+  final ItemHomepage item;
+  final int index;
+
+  const ItemCard({super.key, required this.item, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = [Colors.blue, Colors.green, Colors.red];
+    final color = colors[index % colors.length]; // Cycle through colors
+
+    return Material(
+      color: color,
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        onTap: () {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
+            );
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
